c836582ea146f9e4e1b38742b1d467761d298975
Vladislav Khorenko <pmvladi@yandex.ru>
2024-08-29 17:35:53 +0300

    added utils

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -3,21 +3,20 @@ import NavItems from "./NavItems";
 import Image from "next/image";
 import logo from "../../public/logo.svg";
 import { Bars3Icon } from "@heroicons/react/20/solid";
+import { smoothScroll, smoothScrollToTop } from "../utils";
+import Link from "next/link";
 
 interface Props {
   openNav: () => void;
 }
 
 const Nav = ({ openNav }: Props) => {
-  const smoothScroll = (event: React.MouseEvent, linkTo: string) => {
-    event.preventDefault();
-    document.querySelector(linkTo)?.scrollIntoView({ behavior: "smooth" });
-  };
-
   return (
     <div className="sticky top-0 z-10 w-full bg-inherit shadow-md">
       <div className="w-[95%] h-[68px] flex items-center justify-between mx-auto text-white">
-        <Image src={logo} alt="Nav Logo" />
+        <Link href="/" onClick={smoothScrollToTop}>
+          <Image src={logo} alt="Nav Logo" />
+        </Link>
         <div className="hidden lg:flex space-x-16">
           {["#whatwaiting", "#info", "#bingo", "#faqs"].map((link, i) => (
             <NavItems
